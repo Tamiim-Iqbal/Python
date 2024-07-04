8df76ccd2630f15e82fb4d0e0881b3404cdb666e
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2024-07-04 09:42:57 +0600

    Strings & Conditionals

--- 2. Strings & Conditional Statements/1_string.py
+++ 2. Strings & Conditional Statements/1_string.py
@@ -1,6 +1,6 @@
 # string writing
-str1 = "This is a string.\n"  #next line -> \n
-str2 = 'String in single quote.\t'  #tab -> \t
+str1 = "This is a string.\n"               #next line -> \n
+str2 = 'String in single quote.\t'         #tab -> \t
 str3 = """This is also a string.\n"""
 print(str1, str2, str3)
 
@@ -8,23 +8,23 @@ print(str1, str2, str3)
 str_1 = "Hello "
 str_2 = "World"
 finalString = str_1 + str_2
-print(finalString)   #output : Hello World
+print(finalString)                         #output : Hello World
 
 # length of string -> len()
 len1 = "Tamim"
-print(len(len1))  #output : 5
+print(len(len1))                           #output : 5
 
 # indexing (position) -> start from 0 
 idx = "Bangladesh"
-print(idx[0])  #output : B
-#idx[1] = "A"   #can't be modified
+print(idx[0])                              #output : B
+#idx[1] = "A"                              #can't be modified
 
 # slicing -> str[starting_idx : ending_idx] (ending idx is not included)
 str = "Bangladesh"
-print(str[6 : 10])  #output : desh
-print(str[ : 6])  #[0:6]
-print(str[6 : len(str)])  #[6:last idx]
+print(str[6 : 10])                         #output : desh
+print(str[ : 6])                           #[0:6]
+print(str[6 : len(str)])                   #[6:last idx]
 
 # slicing : Negative index
 str2 = "Apple"
-print(str2[-4 : -1])  #output: ppl
+print(str2[-4 : -1])                       #output: ppl
--- 2. Strings & Conditional Statements/2_stringFunction.py
+++ 2. Strings & Conditional Statements/2_stringFunction.py
@@ -1,14 +1,14 @@
 str = "i am a coder"
 
-print(str.endswith("er"))  #returns true if strings ends with substr 
+print(str.endswith("er"))              #returns true if strings ends with substr 
 
-print((str.capitalize()))  #capitalize 1st char
-print(str)  #original variable remain unchanged
+print((str.capitalize()))              #capitalize 1st char
+print(str)                             #original variable remain unchanged
 
-print(str.replace("o", "a"))  #replaces all occurrence of old
-print(str)  #original variable remain unchanged
+print(str.replace("o", "a"))           #replaces all occurrence of old
+print(str)                             #original variable remain unchanged
 
-print(str.find("am"))  #returns 1st index of 1st occurrence
-print(str.find("b"))  # -1, means b doesn't exist
+print(str.find("am"))                  #returns 1st index of 1st occurrence
+print(str.find("b"))                   #-1, means b doesn't exist
 
-print(str.count("a"))  #counts the occurrence of substr 
+print(str.count("a"))                  #counts the occurrence of substr 
