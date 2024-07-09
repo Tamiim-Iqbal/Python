00cdf8d4b7b42f11aee77636a2e408adb57d9e79
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2024-07-09 10:23:05 +0600

    Dictionary & Set

--- 4. Dictionary & Set in Python/1_dictionary.py
+++ 4. Dictionary & Set in Python/1_dictionary.py
@@ -1,27 +1,27 @@
 # Dictionaries are used to store data values in key:value pairs
 # Properties : unordered, mutable, don't allow duplicate keys
 info = {
-    "name" : "Tamim",                    #string
-    "age" : 22,                          #int
-    "marks" : [94.4, 89.7],              #list
-    "subjects" : ("python", "c"),        #tuple
+    "name" : "Tamim",                      #string
+    "age" : 22,                            #int
+    "marks" : [94.4, 89.7],                #list
+    "subjects" : ("python", "c"),          #tuple
 }
 
-print(type(info))                        #output: <class 'dict'>
-print(info)                              #output: {'name': 'Tamim', 'age': 22, 'marks': [94.4, 89.7], 'subjects': ('python', 'c')}
-print(info["name"])                      #output: Tamim
-print(info["marks"])                     #output: [94.4, 89.7]
+print(type(info))                          #output: <class 'dict'>
+print(info)                                #output: {'name': 'Tamim', 'age': 22, 'marks': [94.4, 89.7], 'subjects': ('python', 'c')}
+print(info["name"])                        #output: Tamim
+print(info["marks"])                       #output: [94.4, 89.7]
 
 #change value
 info["name"] = "Iqbal"
-print(info["name"])                      #output: Iqbal
+print(info["name"])                        #output: Iqbal
 
 #add new value
 info["country"] = "Bangladesh"
-print(info["country"])                   #output: Bangladesh
+print(info["country"])                     #output: Bangladesh
 
 #empty dictionary
 hello = {}
-print(hello)                            #output: {}
+print(hello)                              #output: {}
 hello["text"] = "World"
-print(hello)                            #output:  {'text': 'World'}
+print(hello)                              #output:  {'text': 'World'}
--- 4. Dictionary & Set in Python/2_nestedDictionary.py
+++ 4. Dictionary & Set in Python/2_nestedDictionary.py
@@ -7,6 +7,6 @@ student = {
     }
 }
 
-print(student)  #{'name': 'Saimon', 'marks': {'phy': 99, 'chem': 89, 'math': 95}}
-print(student["marks"])  #{'phy': 99, 'chem': 89, 'math': 95}
-print(student["marks"]["phy"])  #99
+print(student)                              #output: {'name': 'Saimon', 'marks': {'phy': 99, 'chem': 89, 'math': 95}}
+print(student["marks"])                     #output: {'phy': 99, 'chem': 89, 'math': 95}
+print(student["marks"]["phy"])              #output: 99
